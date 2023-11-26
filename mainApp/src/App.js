36eb7b2a2e0416c82f9659443af323e0f067d6f4
mhnipu <mazedul0191.nipu@gmail.com>
2023-11-26 17:52:09 +0600

    modified

---
+++ mainApp/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Header from '../src/components/Header'
+import Footer from '../src/components/Footer'
+import Home from '../src/pages/Home'
+import Products from '../src/pages/Products'
+import ProductDetails from '../src/pages/ProductDetails'
+import Search from '../src/pages/Search'
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/products/:id', element: <Products /> },
+      { path: '/product/:id', element: <ProductDetails /> },
+      { path: '/search', element: <Search /> },
+    ]
+  }
+])
+const App = () => {
+  return <div>
+    <RouterProvider router={router} />
+  </div>;
+};
+
+export default App;
