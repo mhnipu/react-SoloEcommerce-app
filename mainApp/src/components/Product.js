89f94e6c4e6bcfd44e6dfa17e86379093680f57d
mhnipu <mazedul.hoque@televersant.com>
2023-11-28 12:08:30 +0600

    api fixed

--- mainApp/src/components/Product.js
+++ mainApp/src/components/Product.js
@@ -1,7 +1,13 @@
 import React from 'react';
-
-const Product = () => {
-  return <div>Product</div>;
+import { Link } from 'react-router-dom';
+const Product = ({ product }) => {
+  return <Link>
+    <div>
+      <div>
+        <img src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} alt="" />
+      </div>
+    </div>
+  </Link>;
 };
 
 export default Product;
