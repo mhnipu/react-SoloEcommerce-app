b50ee7ca3c039ab7316ba5c99358aa1082240edd
mhnipu <mazedul.hoque@televersant.com>
2023-11-29 12:09:13 +0600

    fixing

--- mainApp/src/components/Product.js
+++ mainApp/src/components/Product.js
@@ -1,13 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// Importing necessary modules/components from React and React Router
+import React from "react";
+import { Link } from "react-router-dom";
+
+// Creating a functional component called Product that takes a 'product' object as a prop
 const Product = ({ product }) => {
-  return <Link>
-    <div>
-      <div>
-        <img src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} alt="" />
+  return (
+    // Wrapping the content in a React Router Link component, though the 'to' attribute is missing here
+    <Link to={`/products/${product.id}`}>
+      {/* Container div for the product */}
+      <div className="grad w-full h-[362px] group rounded-t-lg overflow-hidden relative">
+        {/* Checking if the product has an attribute 'isNew' to display a 'new' label */}
+        {product.attributes.isNew ? (
+          <div className="absolute bg-accent text-primary text-[10px] font-extrabold uppercase top-2 right-2 px-2 rounded-full z-10">
+            new
+          </div>
+        ) : (
+          // Empty string if product is not new
+          ""
+        )}
+        {/* Container for the product image */}
+        <div className="w-full h-[200px] flex items-center justify-center relative">
+          {/* Displaying the product image */}
+          <img
+            className="w-[160px] h-[160px] group-hover:scale-110 transition-all duration-500"
+            src={`http://localhost:1337${product.attributes.image.data.attributes.url}`}
+            alt=""
+          />
+        </div>
+        {/* text */}
+        <div className="px-3 pb-8 flex flex-col">
+          <div className="text-sm text-accent capitalize mb-2">
+            {product.attributes.categories.data[0].attributes.title}
+          </div>
+          <div className="text-[15px] mb-2 lg:mb-6">
+            {product.attributes.title.substring(0, 35)}. . .
+          </div>
+          <div className="text-lg text-accent">
+            ${product.attributes.price}
+          </div>
+        </div>
       </div>
-    </div>
-  </Link>;
+    </Link>
+  );
 };
 
+// Exporting the Product component as default
 export default Product;
